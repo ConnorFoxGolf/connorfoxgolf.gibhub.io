a420742ec4734ec25aaa07f2ea9fb18a6885ed55
Andrew M. Vernier <amverni@umich.edu>
2022-02-21 09:48:31 -0500

    Remove old files

--- src/components/swingVideos/index.tsx
+++ src/components/swingVideos/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import SectionHeader from 'components/shared/sectionHeader';
 
 export default function SwingVideos() {
   return (
     <>
+      <SectionHeader header="Swing Videos" />
       <p>Swing Videos</p>
     </>
   );
